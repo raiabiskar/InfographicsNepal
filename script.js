9c909fdc79aeec947ccd1d01290bfbd8ffee34bb
raiabiskar <raiaabhiskar@gmail.com>
2019-05-23 21:14:08 +0545

    3rd commit

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ t2.from("#header", 1, { opacity: 0, scale: 1 });
 t2.from("#edition", 1, { opacity: 0, scale: 1 });
 t2.from("#goal", 1, { opacity: 0, scale: 1 });
 t2.from("#best", 1, { opacity: 0, scale: 1 });
+
+const divSelector = document.querySelector(".card-left");
+const btn = document.querySelector(".play-svg");
+
+var t3 = new TimelineMax({ paused: true });
+t3.from("#bg-fine", 1, { opacity: 0 });
+t3.from("#policeman", 2.5, { opacity: 0, scale: 1 });
+t3.from("#fine1", 2.5, { opacity: 0, scale: 1, ease: Bounce.easeIn, y: -500 });
+t3.from("#fine2", 2.5, { opacity: 0, scale: 1, ease: Bounce.easeIn, y: -500 });
+t3.from("#fine3", 2.5, { opacity: 0, scale: 1, ease: Bounce.easeIn, y: -500 });
+t3.to("#bg-fine", 1, { opacity: 1 });
+
+btn.addEventListener("click", () => {
+  t3.play();
+});
